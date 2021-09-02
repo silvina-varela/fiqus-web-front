047ff0a79b33c8eb0afc7b9ff4b9a2eef6332221
RomiGomez <[romina.gomez@nayra.coop]>
2021-09-02 15:04:31 -0300

    initial components structure and dependencies

---
+++ src/components/modules/SuccessCase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessCase = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SuccessCase;
---
+++ src/pages/BlogArticle.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlogArticle = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BlogArticle;
