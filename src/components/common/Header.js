131e63fe7ef573965b7c61651ce6485b380e4d2f
RomiGomez <[romina.gomez@nayra.coop]>
2021-09-14 17:22:21 -0300

    router + content.json

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const NavWrapper = styled.nav`
+    width: 100%;
+    height: 80px;
+    background: pink;
+`
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <NavWrapper>
+            <ul>
+                <li><Link to="/"> Home </Link></li>
+                <li><Link to="/Servicios"> Servicios </Link></li>
+                <li><Link to="/Cultura"> Cultura </Link></li>
+                <li><Link to="/Labs"> Labs </Link></li>
+                <li><Link to="/Blog"> Blog </Link></li>
+            </ul>
+        </NavWrapper>
     );
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
-import * as React from "react"
+import React, { useState } from "react"
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route 
+} from 'react-router-dom'
+
 import Header from '../components/common/Header'
 import Footer from '../components/common/Footer'
 import Homepage from './Homepage'
 import Services from './Services'
+import Culture from './Culture'
+import Labs from './Labs'
 import Blog from './Blog'
+import content from '../data/sectionsContent/content.json'
+
 
 import styled from 'styled-components'
 require('../styles/styles.css')
@@ -14,13 +24,33 @@ const Main = styled.main`
   height:100%;
 `
 
-
-
 const IndexPage = () => {
+
+  
+
   return (
     <Main>
-      <Header>BLABLABLABLABLABA</Header>
-      <Homepage> </Homepage>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/">
+            <Homepage content={content.sections[0]}></Homepage>
+          </Route>
+          <Route path="/Servicios">
+            <Services content={content.sections[1]}></Services>
+          </Route>
+          <Route path="/Cultura">
+            <Culture content={content.sections[2]}></Culture>
+          </Route>
+          <Route path="/Labs">
+            <Labs content={content.sections[3]}></Labs>
+          </Route>
+          <Route path="/Blog">
+            <Blog></Blog>
+          </Route>
+        </Switch>
+
+      </Router>
       <Footer></Footer>
     </Main>
   )
