137acba87394eb7f494518ab6a7f9f6a58885a47
RomiGomez <[romina.gomez@nayra.coop]>
2021-09-20 11:07:17 -0300

    import fixes

--- src/pages/Culture.js
+++ src/pages/Culture.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 import Wrapper from '../components/common/PageWrapper'
 import data from '../content/content.json'
