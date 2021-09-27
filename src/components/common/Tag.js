b6e78bb50d00fe5bc3fb83a7df1f8f70e2fcd551
unknown <tobias.irigoyen@nayra.coop>
2021-09-27 15:52:43 -0300

    tag component styles

--- src/components/common/Tag.js
+++ src/components/common/Tag.js
@@ -3,24 +3,23 @@ import styled from 'styled-components'
 
 const TagContainer = styled.div`
     display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
 `
 const TagItem = styled.div`
+    margin: 0 6px 8px 0px;
     padding: 9px 14px;
     border-radius: 10px;
     font-size: 14px;
-    font-weight: ${props => props.theme.fontWeight.medium};
-    color: ${props => props.type === "green" ? props.theme.colors.ultraDarkGrey : props.theme.colors.purplePrimary };
+    font-weight: ${props => props.type === "green" ? props.theme.fontWeight.regular : props.theme.fontWeight.medium };
     background-color: ${props => props.type === "green" ? props.theme.colors.greenLight :  props.theme.colors.purpleLight };
-    
-    @media only screen and (max-width:${props => props.theme.breakpoints.m}px){
-        background: red;
-    }
+    color: ${props => props.type === "green" ? props.theme.colors.ultraDarkGrey : props.theme.colors.purplePrimary };
 `
 
 const Tag = (props) => {
     return (
         <TagContainer>
-            <TagItem  type={ButtonStyles(props.type)} theme={props.theme}>Esto es un tag</TagItem>
+            <TagItem  type={(props.type)} theme={props.theme}>Esto es un tag</TagItem>
         </TagContainer>
     );
 };
