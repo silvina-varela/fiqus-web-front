561f90ce4d97ef95b39f55fff3a86710214d136c
RomiGomez <[romina.gomez@nayra.coop]>
2021-09-28 12:46:39 -0300

    ctreated blog and labs page

---
+++ src/pages/blog.js
@@ -0,0 +1,15 @@
+import React, { Fragment }  from 'react'
+import styled from 'styled-components'
+import Wrapper from '../components/layout/PageWrapper'
+
+
+const Blog = () => {
+
+      return (
+          <Wrapper>
+
+          </Wrapper>
+      );
+};
+
+export default Blog;
