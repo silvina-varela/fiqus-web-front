49a6a454680e13ad7ee0a0472351217163381ed6
unknown <tobias.irigoyen@nayra.coop>
2021-09-28 07:14:19 -0300

    button component styles

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,10 +1,130 @@
 import React from 'react';
+import {Link} from 'gatsby'
+import styled from 'styled-components'
 
-const Button = () => {
+const BtnContainer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+`
+const BtnImg = styled.img`
+    display: ${props => props.type.imgDisplay};
+    height: 20px;
+    margin-right: 6px;
+`
+const Btn = styled(Link)`
+    margin: 0 6px 8px 0px;
+    padding: 9px 20px;
+    font-size: 18px;
+    font-size: ${props => props.type.fontSize};
+    font-weight: ${props => props.type.fontWeight};
+    color: ${props => props.type.color };
+    background-color: ${props => props.type.background};
+    border-radius: 12px;
+    border-color: ${props => props.type.borderColor};
+    border-width: 2px!important;
+    border-style: solid;
+    box-shadow:  0px 4px 0px ${props => props.type.boxShadow};
+    transition: 100ms ease-in-out all;
+    margin-bottom: 10px;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+        box-shadow: none;
+    }
+`
+
+const Button = (props) => {
+    
+    const getBtnStyles = (type) =>{
+        switch (type) {
+            case "btnPrimaryOrange" : 
+                return {
+                    background : props.theme.colors.orangeMain,
+                    borderColor : props.theme.colors.darkMainBg,
+                    boxShadow : props.theme.colors.darkMainBg, 
+                    fontWeight : props.theme.fontWeight.bold, 
+                    color : props.theme.colors.white,
+                    imgDisplay : 'none'
+                 }
+            break
+            case "btnPrimaryWhite" : 
+                return {
+                    background : props.theme.colors.white,
+                    borderColor : props.theme.colors.darkMainBg,
+                    boxShadow : props.theme.colors.darkMainBg, 
+                    fontWeight : props.theme.fontWeight.bold, 
+                    color : props.theme.colors.darkMainBg,
+                    imgDisplay : 'none'
+                }
+            break
+            case "btnPrimaryOrangePurple" : 
+                return {
+                    background : props.theme.colors.orangeMain,
+                    borderColor : props.theme.colors.purplePrimary,
+                    boxShadow : props.theme.colors.purplePrimary, 
+                    fontWeight : props.theme.fontWeight.bold, 
+                    color : props.theme.colors.white,
+                    imgDisplay : 'none'
+                 }
+            break
+            case "btnPrimaryPurple" : 
+                return {
+                    background : props.theme.colors.white,
+                    borderColor : props.theme.colors.purplePrimary,
+                    boxShadow : props.theme.colors.purplePrimary, 
+                    fontWeight : props.theme.fontWeight.bold, 
+                    color : props.theme.colors.purplePrimary,
+                    imgDisplay : 'none'
+                }
+            break
+            case "btnSecondary" : 
+                return {
+                    background : props.theme.colors.white,
+                    borderColor : props.theme.colors.lightGrey,
+                    boxShadow : props.theme.colors.transparent, 
+                    fontSize: '14px',
+                    fontWeight : props.theme.fontWeight.medium, 
+                    color : props.theme.colors.darkGrey,
+                    imgDisplay : 'none'
+                 }
+            break
+            case "btnLabeled" : 
+                return {
+                    background : props.theme.colors.white,
+                    borderColor : props.theme.colors.darkMainBg,
+                    boxShadow : props.theme.colors.darkMainBg, 
+                    fontWeight : props.theme.fontWeight.bold, 
+                    color : props.theme.colors.darkMainBg,
+                    imgDisplay : 'static'
+                 }
+            break
+            default : 
+                return {
+                    background : props.theme.colors.white,
+                    borderColor : props.theme.colors.darkMainBg,
+                    boxShadow : props.theme.colors.darkMainBg,
+                    fontWeight : props.theme.fontWeight.bold, 
+                    color : props.theme.colors.darkMainBg,
+                    imgDisplay : 'none'
+                }
+        }   
+    }
+    
     return (
-        <div>
+        <BtnContainer>   
+            <Btn type={getBtnStyles(props.type)} theme={props.theme} to={props.href}>
+                <BtnImg src={require('../../images/icon_website.svg').default} />
+                ver sitio
+            </Btn>         
+            <Btn type={getBtnStyles(props.type)} theme={props.theme} to={props.href}>
+                <BtnImg src={require ('../../images/icon_github.svg').default} />
+                ir a Github
+            </Btn>
             
-        </div>
+        </BtnContainer>
     );
 };
 
--- src/components/common/PageWrapper.js
+++ src/components/common/PageWrapper.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import Header from '../common/Header'
 import Footer from '../common/Footer'
 import Tag from '../common/Tag'
+import Button from '../common/Button'
 import data from '../../content/content.json'
 
 const styles = data.styles
@@ -28,6 +29,12 @@ const PageWrapper = (props) => {
                 <Header></Header>
                 <PageContainer>{props.children}
                 <Tag type="services" theme={styles}></Tag>
+                <Button type="btnPrimaryOrange" theme={styles} href=""></Button>
+                <Button type="btnPrimaryOrangePurple" theme={styles} href=""></Button>
+                <Button type="btnPrimaryWhite" theme={styles} href=""></Button>
+                <Button type="btnPrimaryPurple" theme={styles} href=""></Button>
+                <Button type="btnSecondary" theme={styles} href=""></Button>
+                <Button type="btnLabeled" theme={styles} href=""></Button>
                 </PageContainer>
                 <Footer></Footer>
             </Wrapper>
