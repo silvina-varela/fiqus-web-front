8287395a231a4eae45b0e811439914caa5e85732
Martina Colmenero <martina.colmenero@nayra.coop>
2021-10-12 13:19:21 -0300

    add intl for translations

--- src/components/modules/Service.js
+++ src/components/modules/Service.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 import Tags from '../common/Tags'
+import { useIntl } from "gatsby-plugin-react-intl"
 
 const ServiceWrapper = styled.div`
     
@@ -14,12 +15,14 @@ const ImageContainer = styled.div`
 
 
 const Service = (props) => {
-    const service = props.service
+    const service = props.service;
+    const intl = useIntl();
+
     return (
         <ServiceWrapper> 
             <TextContainer>
-                <h3>{service.service}</h3>
-                <p>{service.description}</p>
+                <h3>{intl.formatMessage({id:`${props.id}.service`})}</h3>
+                <p>{intl.formatMessage({id: `${props.id}.description`})}</p>
                 <Tags tagsType="services" tags={service.tags} styles={props.styles} ></Tags>
             </TextContainer>
             <ImageContainer>
--- src/pages/servicios.js
+++ src/pages/servicios.js
@@ -1,18 +1,22 @@
 import React, {Fragment} from "react"
 import styled from 'styled-components'
 import { styles, services } from '../content/content.json'
+import { useIntl } from "gatsby-plugin-react-intl"
 
 import Service from '../components/modules/Service'
 
 const Services = () => {
+  const intl = useIntl();
 
-      return (
+    return (
           <Fragment>
-            <h1>{services.title}</h1>
-            {services.services.map( (service) =>{
+            <h1>{intl.formatMessage({id: "services.title"})}</h1>
+            {services.services.map( (service ) =>{
               return(
                 <Service 
-                  key = { service.image } 
+                  key = {service.id}
+                  image = { service.image } 
+                  id = {service.id}
                   service = { service }
                   styles = { styles }>
                 </Service>
