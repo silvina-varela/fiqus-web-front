7fddfa5796ebd794c10ba475dcfe82aed90a6a07
Martina Colmenero <martina.colmenero@nayra.coop>
2021-10-13 15:30:22 -0300

    add button click handling functionality

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -40,10 +40,10 @@ const setSharedStyles = (type) => {
 } ;
 
 const BtnLink = styled(Link)`
-    ${props => setSharedStyles(props.btnStyles)} 
+    ${props => setSharedStyles(props.$btnStyles)} 
 `
 const BtnCta = styled.button`
-    ${props => setSharedStyles(props.btnStyles)} 
+    ${props => setSharedStyles(props.$btnStyles)} 
 `
 
 const Button = (props) => {    
@@ -121,17 +121,22 @@ const Button = (props) => {
                 }
         }   
     }
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        if (props.onButtonClick) props.onButtonClick(event);
+    }
     
     return (
         <BtnContainer>   
             {
                 props.isLink ? 
-                    <BtnLink btnStyles={getBtnStyles(props.type)} theme={props.theme} to={props.href}>
+                    <BtnLink $btnStyles={getBtnStyles(props.type)} theme={props.theme} to={props.href}>
                         <BtnImg imgDisplay={getBtnStyles(props.type).imgDisplay} src={require('../../images/icon_website.svg').default} />
                         {props.btnText}
                     </BtnLink>                     
                 : 
-                    <BtnCta btnStyles={getBtnStyles(props.type)} theme={props.theme}>
+                    <BtnCta $btnStyles={getBtnStyles(props.type)} theme={props.theme} onClick={handleClick}>
                         <BtnImg imgDisplay={getBtnStyles(props.type).imgDisplay} src={require('../../images/icon_website.svg').default} />
                         {props.btnText}
                     </BtnCta>
