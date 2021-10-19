d82ad94ad497e9c50b3352860c0605d62ecc1ce8
Tobias Irigoyen <tobias.irigoyen@nayra.coop>
2021-10-19 14:39:33 -0300

    culture facttic block

--- src/pages/cultura.js
+++ src/pages/cultura.js
@@ -120,7 +120,7 @@ const Culture = () => {
         background-color: ${styles.colors.purplePrimary};
         display: flex;
         flex-direction: column;
-        padding: 30px 20px 12px 20px;
+        padding: 30px 20px 10px 20px;
         @media (min-width: ${styles.breakpoints.m}px) {
             padding-top: 53px;
             padding-bottom: 37px;
@@ -193,6 +193,101 @@ const Culture = () => {
         max-width: 754px;
         margin: auto;
     }
+    `    
+    const FactticMainContainer = styled.div`
+        background-color: ${styles.colors.white};
+        padding: 30px 20px 40px 20px;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            padding-top: 50px;
+            padding-bottom: 80px;
+        }
+    `
+    const FactticTitle = styled.h2`
+        font-size: 1.44em;
+        line-height: 34px;
+        font-weight: ${styles.fontWeight.bold};
+        color: ${styles.colors.purplePrimary};
+        margin-bottom: 10px;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            font-size: 1.72em;
+            font-weight: ${styles.fontWeight.bold};
+            margin-bottom: 17px;
+        }
+    `
+    const FactticLead = styled.h3`
+        font-size: 1em;
+        line-height: 26px;
+        font-weight: ${styles.fontWeight.regular};
+        color: ${styles.colors.purplePrimary};
+        margin-bottom: 15px;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            font-size: 1.44em;
+            line-height: 34px;
+            font-weight: ${styles.fontWeight.regular};  
+            margin-bottom: 15px;
+        }
+    `
+    const FactticContentContainer = styled.div`
+        padding: 0;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            max-width: 754px;
+            margin: auto;
+            margin-bottom: 4px;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+    `
+    const FactticContentLeftBlock = styled.div`
+        @media (min-width: ${styles.breakpoints.m}px) {
+            flex-basis: calc(50% - 9px);
+            max-width: 370px;
+            margin-right: 15px;
+        }
+    `
+    const FactticContentRightBlock = styled.div`
+        margin-bottom: 27px;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            flex-basis: calc(50% - 8px);
+            max-width: 370px;
+        }
+    `
+    const FactticContentFullBlock = styled.div`
+        @media (min-width: ${styles.breakpoints.m}px) {
+            width: 100%;
+            max-width: 754px;
+            margin: auto;
+        }
+    `    
+    const FactticSubtitle = styled.h3`
+        font-size: 1.44em;
+        line-height: 34px;
+        font-weight: ${styles.fontWeight.bold};
+        color: ${styles.colors.purplePrimary};
+        text-align: left;
+        margin-bottom: 18px;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            font-size: 1.72em;
+            line-height: 32px;
+            font-weight: ${styles.fontWeight.bold};  
+            margin-bottom: 15px;
+        }
+    `
+    const FactticContent = styled.p`
+        font-size: .88em;
+        line-height: 22px;
+        color: ${styles.colors.darkMainBg};
+        margin-bottom: 22px;
+        @media (min-width: ${styles.breakpoints.m}px) {
+            font-size: 1em;
+            line-height: 26px;
+            margin-bottom: 25px;
+        }
+    `
+    
+    const FactticContentBoldText = styled.b`
+        //font-weight: ${styles.fontWeight.regular};
     `
 
     const history = intl.formatMessage({id: "culture_historia.content_col1"});
@@ -208,8 +303,8 @@ const Culture = () => {
             <HistoryMainContainer>
                 <HistoryTitle>{intl.formatMessage({id: "culture_historia.title"})}</HistoryTitle>
                 <TeamImgContainer>
-                    <TeamImgMobile src={require('../images/'+intl.formatMessage({id: "culture_historia.imageMobile"})+'.png').default} alt="equipo Fiqus" />
-                    <TeamImgDesktop src={require('../images/'+intl.formatMessage({id: "culture_historia.imageDesktop"})+'.png').default} alt="equipo Fiqus" />
+                    <TeamImgMobile src={require('../images/'+ intl.formatMessage({id: "culture_historia.imageMobile"}) + '.png').default} alt="equipo Fiqus" />
+                    <TeamImgDesktop src={require('../images/'+ intl.formatMessage({id: "culture_historia.imageDesktop"}) + '.png').default} alt="equipo Fiqus" />
                 </TeamImgContainer>
                 <HistoryContentContainer>
                     <HistoryContentLeftBlock>
@@ -250,7 +345,7 @@ const Culture = () => {
             <FederalMainContainer>
                 <TopBlock>
                     <TopBlockImgContainer>
-                        <TopBlockImg src={require('../images/illustrations/'+intl.formatMessage({id: "culture_federales.image"})+'.svg').default} alt="mapa argentina" />
+                        <TopBlockImg src={require('../images/illustrations/'+ intl.formatMessage({id: "culture_federales.image"}) + '.svg').default} alt="mapa argentina" />
                     </TopBlockImgContainer>
                     <TopBlockInfoContainer>
                         <FederalSubtitle>{intl.formatMessage({id: "culture_federales.title1"})}</FederalSubtitle>
@@ -294,6 +389,32 @@ const Culture = () => {
 
 
             {/* Cultura | Estamos Federados */}
+            
+            <FactticMainContainer>
+                <FactticContentContainer>
+                    <FactticTitle>{intl.formatMessage({id: "culture_facttic.title"})}</FactticTitle>
+                    <FactticLead>{intl.formatMessage({id: "culture_facttic.subtitle"})}</FactticLead>
+                    <FactticContentLeftBlock>
+                        <FactticContent>
+                            {intl.formatMessage({id: "culture_facttic.content_line1_part1"})}
+                            <FactticContentBoldText>{intl.formatMessage({id: "culture_facttic.content_line1_facttic"})}</FactticContentBoldText>
+                            {intl.formatMessage({id: "culture_facttic.content_line1_part2"})}
+                        </FactticContent>
+                    </FactticContentLeftBlock>
+                    <FactticContentRightBlock>
+                        <FactticContent>{intl.formatMessage({id: "culture_facttic.content_line2"})}</FactticContent>
+                        <FactticContent>{intl.formatMessage({id: "culture_facttic.content_line3"})}</FactticContent>
+                    </FactticContentRightBlock>
+                </FactticContentContainer>
+                <FactticContentFullBlock>
+                    <FactticSubtitle>{intl.formatMessage({id: "culture_internacional.title"})}</FactticSubtitle>
+                    <FactticContent>{intl.formatMessage({id: "culture_internacional.content_line1"})}</FactticContent>
+                    <FactticContent>{intl.formatMessage({id: "culture_internacional.content_line2"})}</FactticContent>
+                    <FactticContent>{intl.formatMessage({id: "culture_internacional.content_line3"})}</FactticContent>
+                </FactticContentFullBlock>
+            </FactticMainContainer>
+            
+            {/*
             <div>
                 <h2>{intl.formatMessage({id: "culture_facttic.title"})}</h2>
                 <h2>{intl.formatMessage({id: "culture_facttic.subtitle"})}</h2>
@@ -313,7 +434,7 @@ const Culture = () => {
                 <p>{intl.formatMessage({id: "culture_internacional.content_line1"})}</p>
                 <p>{intl.formatMessage({id: "culture_internacional.content_line2"})}</p>
             </div>
-
+            */}
         </Fragment>
     );
 };
