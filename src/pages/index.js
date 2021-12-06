9e363c351b52dc1cd675dcd3ffd5f3fbbae1c0a0
Romi Gomez <romina.gomez@nayra.coop>
2021-12-06 20:51:02 -0300

    homepage services fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,17 @@
-import React, {useContext, Fragment} from "react"
+import React, {Fragment} from "react"
 import { useIntl } from "gatsby-plugin-react-intl"
 import HomepageHeader from "../components/modules/HomepageHeader"
 import HomepageCulture from "../components/modules/HomepageCulture"
 import HomepageLabs from "../components/modules/HomepageLabs"
 import Services from "./servicios"
-import isHomepageContext from "../context/isHomepageContext"
 
 const Index = (props) => {
   const intl = useIntl();
-  const ishomepage = useContext(isHomepageContext)
 
   return (
     <Fragment>
         <HomepageHeader/>
-        <Services ishomepage={ishomepage} />
+        <Services ishomepage="true" />
         <HomepageCulture />
         <HomepageLabs/>
     </Fragment>
